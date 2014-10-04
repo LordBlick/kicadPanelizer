ac5b76fec07c773b363f77b535a32e26cba76667
LordBlick <LordBlick@gmail.com>
2014-10-04 16:19:40 +0200

    User interface cosmetics

--- uiPanelize.py
+++ uiPanelize.py
@@ -14,8 +14,12 @@ class EasyTextView(gtk.TextView):
 		self.get_buffer().set_text('')
 
 	def set_size_request(self, x, y):
-		parent = self.get_parent()
-		if parent and(isinstance(parent, gtk.ScrolledWindow)):
+		try:
+			parent = self.get_parent()
+			bPass = True
+		except AttributeError, e:
+			bPass = False
+		if bPass and(isinstance(parent, gtk.ScrolledWindow)):
 			parent.set_size_request(x, y)
 		else:
 			super(EasyTextView, self).set_size_request(x, y)
@@ -60,7 +64,7 @@ class EasyTextView(gtk.TextView):
 		return pangoTabSpc
 
 class apw:
-	def Label(self, txtLabel, hFixed, posX, poxY, width, height=None, fontDesc=None, xalign=None, selectable=False):
+	def Label(self, txtLabel, hFixed, posX, posY, width, height=None, fontDesc=None, xalign=None, selectable=False):
 		if not height:
 			height=self.Height
 		hLabel = gtk.Label(txtLabel)
@@ -74,10 +78,10 @@ class apw:
 		hLabel.show()
 		hLabel.set_size_request(width, height)
 		if hFixed:
-			hFixed.put(hLabel, posX, poxY)
+			hFixed.put(hLabel, posX, posY)
 		return hLabel
 
-	def Num(self, numTup, hFixed, posX, poxY, width, partDigits=0, height=None, fontDesc=None):
+	def Num(self, numTup, hFixed, posX, posY, width, partDigits=0, height=None, fontDesc=None):
 		if not height:
 			height=self.Height
 		numInit, numMin, numMax, numStep = numTup
@@ -89,10 +93,10 @@ class apw:
 			hSpin.modify_font(fontDesc)
 		hSpin.set_size_request(width, height)
 		hSpin.set_update_policy(gtk.UPDATE_IF_VALID)
-		hFixed.put(hSpin, posX, poxY)
+		hFixed.put(hSpin, posX, posY)
 		return hSpin
 
-	def ComboBox(self, modelCb, hFixed, posX, poxY, width, height=None, fontDesc=None, wrap=None, selTxt=0):
+	def ComboBox(self, modelCb, hFixed, posX, posY, width, height=None, fontDesc=None, wrap=None, selTxt=0):
 		if not height:
 			height=self.Height
 		hCb = gtk.ComboBox()
@@ -107,10 +111,10 @@ class apw:
 			crtCb.set_property('ellipsize', pango.ELLIPSIZE_END)
 		hCb.set_model(modelCb)
 		hCb.set_size_request(width, height+4)
-		hFixed.put(hCb, posX, poxY-2)
+		hFixed.put(hCb, posX, posY-2)
 		return hCb
 
-	def Butt(self, txtLabel, hFixed, posX, poxY, width, height=None, fileImage=None, stockID=None, fontDesc=None):
+	def Butt(self, txtLabel, hFixed, posX, posY, width, height=None, fileImage=None, stockID=None, fontDesc=None):
 		"""If stockID is set, txtLabel set as True means full stock button,
 		non-null string - own Label for stock image,
 		in other case - button with only stock image"""
@@ -146,10 +150,10 @@ class apw:
 				hButt = gtk.Button()
 				hButt.add(image)
 		hButt.set_size_request(width, height)
-		hFixed.put(hButt, posX, poxY)
+		hFixed.put(hButt, posX, posY)
 		return hButt
 
-	def TextView(self, hFixed, posX, poxY, width, height, bWrap=False, bEditable=True, tabSpace=2, fontDesc=None):
+	def TextView(self, hFixed, posX, posY, width, height, bWrap=False, bEditable=True, tabSpace=2, fontDesc=None):
 		hTextView = EasyTextView()
 		hTextView.set_property("editable", bEditable)
 		if fontDesc:
@@ -163,7 +167,7 @@ class apw:
 		scrollViewTxt.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
 		scrollViewTxt.add(hTextView)
 		scrollViewTxt.set_size_request(width, height)
-		hFixed.put(scrollViewTxt, posX, poxY)
+		hFixed.put(scrollViewTxt, posX, posY)
 		return hTextView
 
 	def dialogChooseFile(self, parent=None, startDir=None, startFile=None, title='Select a file...', act='file_open', bShowHidden=False):
@@ -191,6 +195,70 @@ class apw:
 		hDialog.destroy()
 		return fileName
 
+class radioFrame(gtk.Frame):
+	def __init__(frame, txtLabel, parentFixed, lsVal, x, y, row_height, active=0, wrap=0, fontDesc=None):
+		super(gtk.Frame, frame).__init__(label=txtLabel)
+		items = len(lsVal)
+		frame.fixed = gtk.Fixed()
+		frame.lsRet = map(lambda row: row[1], lsVal)
+		if type(wrap) is not int or(wrap<0):
+			wrap = 0
+		frameW = 5
+		maxColW = 0
+		posX = 2
+		for idx, (radioTxt, radioValue) in enumerate(lsVal):
+			if idx:
+				hRadio = gtk.RadioButton(group=hMainRadio, label=radioTxt)
+			else:
+				hRadio = hMainRadio = gtk.RadioButton(group=None, label=radioTxt)
+			if fontDesc:
+				hRadio.modify_font(fontDesc)
+			radioW = frame.getTxtPixelWidth(hRadio, radioTxt, fontDesc=fontDesc)+20
+			if wrap:
+				maxColW = max(maxColW, radioW)
+				if not(idx%wrap) or(idx==items-1) and(items%wrap):
+					frameW += maxColW+2
+				if idx and(not(idx%wrap)):
+					posX += maxColW+2
+					maxColW = 0
+				posY = (idx%wrap)*row_height
+				if idx==items-1: frameW += 3
+			else:
+				posY = idx*row_height
+				frameW = max(frameW, radioW)
+				if idx==items-1: frameW += 5
+			hRadio.set_size_request(radioW, row_height-2)
+			frame.fixed.put(hRadio, posX, posY)
+			hRadio.connect("toggled", frame.callBack, radioValue)
+		frameH = (wrap+1)*row_height if wrap else (items+1)*row_height
+		frame.add(frame.fixed)
+		frame.set_size_request(frameW, frameH)
+		parentFixed.put(frame, x, y)
+		frame.group = tuple(reversed(hMainRadio.get_group()))
+		if type(active) is not int or(active>=items):
+			frame.value = None
+			return
+		frame.set_active(active)
+		
+		#frame.value = lsVal[active][1]
+
+	def getTxtPixelWidth(frame, widget, txt, fontDesc=None):
+		pangoLayout = widget.create_pango_layout(txt)
+		if fontDesc:
+			pangoLayout.set_font_description(fontDesc)
+		pangoTxtSpc = pangoLayout.get_pixel_size()[0]
+		del(pangoLayout)
+		return pangoTxtSpc
+
+	def callBack(frame, radio, value):
+		if radio.get_active():
+			frame.value = value
+			frame.active = frame.group.index(radio)
+
+	get_active = lambda frame: frame.active
+	set_active = lambda frame, active: frame.group[active].set_active(True) 
+	get_value = lambda frame: frame.value
+
 class panelizeUI:
 	def __init__(ui):
 		from sys import path as ptSys
@@ -203,7 +271,9 @@ class panelizeUI:
 		if __name__ == "__main__":
 			ui.mainWindow.connect("destroy", lambda w: ui.uiExit())
 			ui.buttonExit.connect("clicked", lambda w: ui.uiExit())
-			ui.logView.insert_end("User Interface Test...\nSo long… So long… So long… So long… long… Sooooo long… ")
+			ui.buttonProceed.connect("clicked",
+				lambda w: ui.logView.insert_end("Angle: %0.1f°\n" % (ui.rfAngle.get_value()/10)))
+			ui.logView.insert_end("User Interface Test...\nSo long… So long… So long… So long… long… Sooooo long…\n")
 			ui.uiEnter()
 			gtk.main()
 
@@ -228,7 +298,7 @@ class panelizeUI:
 		cd(ui.callDir)
 		ui.title = "pcbnew py module based Panelizator. BZR>5160"
 		ui.mainWindow = gtk.Window(gtk.WINDOW_TOPLEVEL)
-		ui.wdhMain, ui.hgtMain = (550, 300)
+		ui.wdhMain, ui.hgtMain = (570, 300)
 		ui.mainWindow.set_geometry_hints(min_width=ui.wdhMain, min_height=ui.hgtMain)
 		ui.mainWindow.set_size_request(ui.wdhMain, ui.hgtMain)
 		ui.mainWindow.set_title(ui.title)
@@ -237,34 +307,28 @@ class panelizeUI:
 		ui.mainWindow.add_accel_group(accGroup)
 		mainFrame = ui.mainFrame = gtk.Fixed()
 
-		apw.Label("Columns:", mainFrame, 5, 5, 45)
-		ui.numCols = apw.Num((1, 1, 1000, 1), mainFrame, 55, 5, 45)
-		apw.Label("Rows:", mainFrame, 110, 5, 30)
-		ui.numRows = apw.Num((1, 1, 1000, 1), mainFrame, 145, 5, 45)
-		apw.Label("Rotate:", mainFrame, 200, 5, 35)
-		ui.lsAngle = gtk.ListStore(goStr, goInt)
-		for n in range(-1, 3):
-			ang = n*90
-			ui.lsAngle.append( ("%i°" % ang, int((ang+360)%360*10)) )
-		ui.cbAngle = apw.ComboBox(ui.lsAngle, mainFrame, 240, 6, 60)
-		ui.cbAngle.set_active(1)
-		
-		ui.numMarginMM = apw.Num((0, 0, 500, 1), mainFrame, 350, 5, 70, partDigits=4)
-		apw.Label("mm=", mainFrame, 422, 5, 23)
-		apw.Label("Margin:", mainFrame, 310, 5, 40)
-		ui.numMarginMils = apw.Num((0, 0, 20000, 10), mainFrame, 445, 5, 70, partDigits=3)
-		apw.Label("mils", mainFrame, 515, 5, 20)
-
-		ui.labFilename = apw.Label("File:", mainFrame, 5, 35, 30)
-		if __name__ == "__main__":
-			ui.txtFilename = apw.Butt('Test', mainFrame, 35, 35, 0)
-		else:
-			ui.txtFilename = apw.Label(u'Drag file to log view or use „Open” button →', mainFrame, 35, 35, 0, xalign=0., selectable=True)
-		ui.buttonFileName = ui.apw.Butt(None, mainFrame, 0, 0, 30, stockID=gtk.STOCK_OPEN)
-		ui.buttonProceed = ui.apw.Butt("Proceed", mainFrame, 0, 0, 80, stockID=gtk.STOCK_MEDIA_PLAY)
-
-		ui.logView = ui.apw.TextView(mainFrame, 5, 65, 0, 0,
+		ui.logView = ui.apw.TextView(mainFrame, 5, 5, 0, 0,
 			bEditable=False, tabSpace=4, fontDesc = ui.fontFixedDesc)
+
+		lsAngle = map(lambda n: ((u"{}°".format(n*90)), (int((n*90+360)%360*10))), (range(-1, 3)))
+		ui.rfAngle = radioFrame("Rotate:", mainFrame, lsAngle, 0, 0, 20,
+			active=1, wrap=2, fontDesc=ui.fontDesc)
+
+		ui.labFilename = apw.Label("File:", mainFrame, 0, 0, 30)
+		if __name__ == "__main__":
+			ui.txtFilename = apw.Butt('Test', mainFrame, 0, 0, 0)
+		else:
+			ui.txtFilename = apw.Label(u'Drag file to log view or use „Open” button →',
+				mainFrame, 0, 0, 0, xalign=0., selectable=True)
+		ui.buttonFileName = ui.apw.Butt(None, mainFrame, 0, 0, 30, stockID=gtk.STOCK_OPEN)
+
+		ui.labMargin = apw.Label("Margin:", mainFrame, 0, 0, 40)
+		ui.numMarginMM = apw.Num((0, 0, 500, 1), mainFrame, 0, 0, 70, partDigits=4)
+		ui.labMarginMM = apw.Label("mm=", mainFrame, 0, 0, 23)
+		ui.numMarginMils = apw.Num((0, 0, 20000, 10), mainFrame, 0, 0, 70, partDigits=3)
+		ui.labMarginMils = apw.Label("mils", mainFrame, 0, 0, 20)
+
+		ui.buttonProceed = ui.apw.Butt("Proceed", mainFrame, 0, 0, 80, stockID=gtk.STOCK_MEDIA_PLAY)
 
 		ui.logoBigPixbuf = gtk.gdk.pixbuf_new_from_file(pt.realpath(pt.expanduser("pics/panelize-pcb.svg")))
 		gtk.window_set_default_icon_list(ui.logoBigPixbuf, )
@@ -272,8 +336,14 @@ class panelizeUI:
 		ui.imageLogo.set_from_pixbuf(ui.logoBigPixbuf)
 		mainFrame.put(ui.imageLogo, 0, 0)
 
+		ui.labCols = apw.Label("Columns:", mainFrame, 0, 0, 45)
+		ui.numCols = apw.Num((1, 1, 999, 1), mainFrame, 0, 0, 37)
+
 		ui.buttonClear = ui.apw.Butt("Clear log view", mainFrame, 0,  0, 80)
 		ui.buttonClear.connect("clicked", lambda xargs: ui.logView.clear_text())
+
+		ui.labRows = apw.Label("Rows:", mainFrame, 0, 0, 30)
+		ui.numRows = apw.Num((1, 1, 999, 1), mainFrame, 0, 0, 37)
 
 		ui.buttonExit = ui.apw.Butt("Exit (Ctrl+Q)", mainFrame, 0, 0, 80)
 		ui.buttonExit.add_accelerator("clicked", accGroup, ord('Q'),
@@ -292,16 +362,31 @@ class panelizeUI:
 				return True
 			stdH = ui.apw.Height
 			ui.lastWinSize = w, h
-			ya = 5
-			yb = 35
+			ui.logView.get_parent().set_size_request(w-20, h-110)
+			#ui.logView.get_parent().set_size_request(w-20, h-250)
+			#y1 = h-130
+			y1 = h-240
+			y2 = h-100
+			ui.mainFrame.move(ui.labFilename, 5, y2)
+			ui.mainFrame.move(ui.txtFilename, 35, y2)
 			ui.txtFilename.set_size_request(w-170, stdH)
-			ui.mainFrame.move(ui.buttonFileName, w-130, yb)
-			ui.mainFrame.move(ui.buttonProceed, w-95, yb)
-			ui.logView.get_parent().set_size_request(w-20, h-130)
+			ui.mainFrame.move(ui.buttonFileName, w-130, y2)
+			ui.mainFrame.move(ui.buttonProceed, w-95, y2)
 			ui.mainFrame.move(ui.imageLogo, 0, h-57)
-			yc = h-37
-			ui.mainFrame.move(ui.buttonClear, w-180, yc)
-			ui.mainFrame.move(ui.buttonExit, w-95, yc)
+			x1, x2, y3 = 90, 435, h-70
+			ui.mainFrame.move(ui.labMargin, x1-40, y3)
+			ui.mainFrame.move(ui.numMarginMM, x1, y3)
+			ui.mainFrame.move(ui.labMarginMM, x1+72, y3)
+			ui.mainFrame.move(ui.numMarginMils, x1+95, y3)
+			ui.mainFrame.move(ui.labMarginMils, x1+165, y3)
+			ui.mainFrame.move(ui.rfAngle, 280, y3-3)
+			ui.mainFrame.move(ui.labCols, x2-47, y3)
+			ui.mainFrame.move(ui.numCols,  x2, y3)
+			ui.mainFrame.move(ui.buttonClear, w-95, y3)
+			y4 = h-40
+			ui.mainFrame.move(ui.labRows, x2-32, y4)
+			ui.mainFrame.move(ui.numRows, x2, y4)
+			ui.mainFrame.move(ui.buttonExit, w-95, y4)
 			return True
 
 	def uiTick(ui):
